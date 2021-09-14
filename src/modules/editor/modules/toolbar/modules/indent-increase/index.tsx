d690b3b80ae990a8a93fa280a968e981cf4ad476
christiandrey <oluwaseun.adedire@gmail.com>
2021-09-14 06:13:35 +0100

    chore: bind indentation update tools

--- src/modules/editor/modules/toolbar/modules/indent-increase/index.tsx
+++ src/modules/editor/modules/toolbar/modules/indent-increase/index.tsx
@@ -1,10 +1,18 @@
 import {IconButton} from '../../../icon-button';
 import {ReactComponent as IndentIncrease} from '../../../../../../assets/images/icon-indent-increase.svg';
 import React from 'react';
+import {changeBlockIndentation} from '../../../../../../packages/common/commands';
+import {useSlateStatic} from 'slate-react';
 
 export const IndentIncreaseButton = () => {
+	const editor = useSlateStatic();
+
+	const handlePress = () => {
+		changeBlockIndentation(editor, 'increment');
+	};
+
 	return (
-		<IconButton tip='Increase indent'>
+		<IconButton onPress={handlePress} tip='Increase indent'>
 			<IndentIncrease />
 		</IconButton>
 	);
