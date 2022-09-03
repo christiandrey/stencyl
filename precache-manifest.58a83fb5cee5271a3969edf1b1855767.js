81ad85bfb9c5149925e8b71521657b960d90ad30
christiandrey <oluwaseun.adedire@gmail.com>
2022-09-03 12:01:30 +0100

    Updates

---
+++ precache-manifest.58a83fb5cee5271a3969edf1b1855767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0564056ab7ea7a790d78ed92ffea52",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f751aa238fdbf9da743",
+    "url": "./static/css/main.c5e27288.chunk.css"
+  },
+  {
+    "revision": "870a8de34298675a33be",
+    "url": "./static/js/2.3c855a6e.chunk.js"
+  },
+  {
+    "revision": "191d4ba22f374cc6168cd027acfa51d8",
+    "url": "./static/js/2.3c855a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f751aa238fdbf9da743",
+    "url": "./static/js/main.5eec2ddd.chunk.js"
+  },
+  {
+    "revision": "67abe3132472b2a20201",
+    "url": "./static/js/runtime-main.cd679b30.js"
+  }
+]);
